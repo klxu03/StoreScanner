2b467b69561ffae46f7c139b39e2dcd9810b6f88
AnuragGowda <gowdaanuragr@gmail.com>
2020-02-29 01:08:05 -0500

    fixed

--- flask_app/store/routes.py
+++ flask_app/store/routes.py
@@ -10,15 +10,15 @@ items = {
 
 @app.route('/item/<name>')
 def getInfo(name):
-    return render_template("index.html", info=items[name])
+    return render_template("item.html", info=items[name]) if name in items else render_template("item.html", info={"name":"Unknown item"})
 
 @app.route('/item')
 def item():
-    return render_template("item.html")
+    return render_template("item.html", info={"name":"Waiting for user input"})
 
 @app.route('/cart')
 def cart():
-    return render_template("item.html")
+    return render_template("cart.html")
 
 @app.route('/')
 def home():
