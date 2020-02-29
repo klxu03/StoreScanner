929d54240d17c38be01e2e2f0d2fcd5ad6df2bb8
AnuragGowda <gowdaanuragr@gmail.com>
2020-02-29 07:44:19 -0500

    .

--- flask_app/store/edamam.py
+++ flask_app/store/edamam.py
@@ -3,11 +3,10 @@ from dotenv import load_dotenv
 from requests import get
 import urllib.parse
 
-load_dotenv() # load app id, key
-
 def nutritional_info(food):
 	"""Return a JSON object with the nutritional info of the given food.
 	Relies on the Edamam API, with API keys in .env."""
+	load_dotenv() # load app id, key
 	url = "https://api.edamam.com/api/nutrition-data"
 	payload = {
 		"app_id": getenv("APP_ID"),
