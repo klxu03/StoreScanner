d002de98b5f6dd3b3c8df92352fc86397ff4bdb8
AnuragGowda <gowdaanuragr@gmail.com>
2020-02-29 12:51:31 -0500

    .

--- flask_app/store/form.py
+++ flask_app/store/form.py
@@ -1,11 +1,10 @@
 # Import flask forms from flask_wtf
 from flask_wtf import FlaskForm
 # Import the types of fields that we want to have, a string, password, submit and boolean feild
-from wtforms import StringField, SubmitField, FileField
+from wtforms import StringField, SubmitField
 
 # Create a new object that inherits the FlaskForm properties, we want to add our additional attributes that this object should have
 class ItemForm(FlaskForm):  
-    picture = FileField()
 #    picture = FileField('Barcode Picture', validators=[FileAllowed(images, 'Only images allowed!')])
     item = StringField('Item', render_kw={"placeholder": "Enter an item"})
     # Add a submit field
